19523da1593b7e84420cc726750a38d4e5b7e077
Denesvargas <37148037+Denesvargas@users.noreply.github.com>
2018-03-31 19:12:54 -0300

    Update t2parte2.hs

--- t2/t2parte2.hs
+++ t2/t2parte2.hs
@@ -18,3 +18,12 @@ isBin' :: String -> Bool
 isBin' str = not(any (\x -> x /= '0' && x /= '1') str) && length str /= 0
 
 -- exercicio 3
+bin2dec :: [Int] -> Int
+bin2dec [] = undefined
+bin2dec bits = auxBin2Dec bits ((length bits)-1)
+
+auxBin2Dec :: [Int] -> Int -> Int
+auxBin2Dec [] _ = 0
+auxBin2Dec (x:xs) y = x*(2^(y)) + auxBin2Dec xs (y-1)
+
+--- exercicio 4
