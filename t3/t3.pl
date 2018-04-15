58704c987277cdc8e5afc94474791eb951f5307b
Denesvargas <37148037+Denesvargas@users.noreply.github.com>
2018-04-15 17:38:57 -0300

    Update t3.pl

--- t3/t3.pl
+++ t3/t3.pl
@@ -6,3 +6,12 @@ has5(L) :- L = [_,_,_,_,_].
 
 % Exercício 3
 hasN(L,N) :- length(L,N).
+
+% Exercício 4
+potN0(0,[1]).
+potN0(N,L) :- 
+    N > 0,
+    L = [H1|T1],
+    H1 is 2**N,
+    N1 is N-1,
+    potN0(N1,T1).
