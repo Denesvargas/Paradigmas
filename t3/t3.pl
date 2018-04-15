0a0c03f383f2f1f30ce33d092ec55a9822ae1705
Denesvargas <37148037+Denesvargas@users.noreply.github.com>
2018-04-15 17:47:00 -0300

    Update t3.pl

--- t3/t3.pl
+++ t3/t3.pl
@@ -15,3 +15,13 @@ potN0(N,L) :-
     H1 is 2**N,
     N1 is N-1,
     potN0(N1,T1).
+
+% Exercício 5
+zipmult([],[],[]).
+zipmult(L1,L2,L3) :-
+    L1 = [H1|T1],
+    L2 = [H2|T2],
+    L3 = [H3|T3],
+    H3 is H1*H2,
+    zipmult(T1,T2,T3).
+
