84073f387ee49f3c524350b90337eab28b81d8bb
Denesvargas <37148037+Denesvargas@users.noreply.github.com>
2018-04-29 17:32:43 -0300

    Create t4a.pl

---
+++ t4/t4a.pl
@@ -0,0 +1,41 @@
+%%% regra 1 
+%cd Independente
+%S ocupa a quarta faixa do CD.
+regra1(L) :- nth1(4,L,s).
+
+% regra 2
+% Tanto W como Y precedem S no CD (ou seja, W e Y estão
+% numa faixa que é tocada antes de S no CD).
+regra2(L) :-
+    nth0(P1,L,w),
+    nth0(P2,L,y),
+    nth0(P3,L,s),
+    P1 < P3,
+    P2 < P3,
+    !.
+
+% regra 3
+% T precede W no CD (ou seja, T está numa faixa que é tocada antes de W).
+regra3(L) :-
+    nth0(P1,L,t),
+    nth0(P2,L,w),
+    P1 < P2,
+    !.
+
+% regra 4
+% Um sucesso de rock ocupa a sexta faixa do CD.
+sucesso(_).
+regra4(L) :- L = [_,_,_,_,_,S,_], sucesso(S).
+
+% regra 5
+%  Cada sucesso de rock é imediatamente precedido no CD por uma
+%  composição da banda (ou seja,no CD cada sucesso de rock toca
+%  imediatamente após uma composição da banda).
+
+banda(_).
+regra5(L) :- sucesso(A), banda(B), nextto(B,A,L).
+    
+% regra 6
+% Z é um sucesso de rock.
+regra6(N) :- sucesso(N).
+    
