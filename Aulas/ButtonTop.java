d4796a8d87fce16d5384dbf7615a21b81aac3963
Denesvargas <37148037+Denesvargas@users.noreply.github.com>
2018-05-26 23:48:36 -0300

    Create ButtonTop.java

---
+++ Aulas/ButtonTop.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+public class ButtonTop {
+  Button cb = new Button("Circle");
+  Button lb = new Button("Line");  
+  
+  public void setPane(BorderPane bp){
+    ArrayList<Button> buttons = new ArrayList();
+    buttons.add(cb);
+    buttons.add(lb);
+    HBox box = new HBox();
+    box.setSpacing(10);
+    box.setAlignment(Pos.CENTER);
+    box.setPadding(new Insets(5,5,5,5));
+    box.setStyle("-fx-background-color: #00008B;");
+    box.getChildren().addAll(buttons);
+    bp.setTop(box);
+  }
+  
+  public void pressiona(Pane panel){
+    cb.setOnMousePressed((MouseEvent e) -> {
+      Desenha circulo = new Desenha();
+      circulo.desenha(1, panel);
+    });
+    lb.setOnMousePressed((MouseEvent e) -> {
+      Desenha circulo = new Desenha();
+      circulo.desenha(2, panel);
+    });
+  }  
+}
