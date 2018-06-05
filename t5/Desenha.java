b334a319a80f28895507716bf82cff310ee799fa
Denesvargas <37148037+Denesvargas@users.noreply.github.com>
2018-06-04 23:46:45 -0300

    Update Desenha.java

--- t5/Desenha.java
+++ t5/Desenha.java
@@ -15,48 +15,52 @@ public class Desenha {
   int ind_noIni = -1;
   double tam = 15;
   
-  public void desenha(int t, Pane panel, ColorPicker cor, Grafo lnos, Aresta lline){
+  public void desenha(int t, Pane panel, ColorPicker cor, Grafo grafo){
     tipo = t;
     panel.setOnMousePressed((MouseEvent e)-> {
       if(tipo == 1){
         c = new Circle(e.getX(), e.getY(), tam, cor.getValue());
-        lnos.add_circle(c);
+        grafo.add_circle(c);
         panel.getChildren().add(c);
       }
       else if(tipo == 2){
-        ind_noIni = lnos.sobre_circ(e.getX(), e.getY());
+        ind_noIni = grafo.sobre_circ(e.getX(), e.getY());
         if(ind_noIni != -1){
-          l = new Line(lnos.posX(ind_noIni), lnos.posY(ind_noIni),lnos.posX(ind_noIni), lnos.posY(ind_noIni));
+          l = new Line(grafo.posX(ind_noIni), grafo.posY(ind_noIni), grafo.posX(ind_noIni), grafo.posY(ind_noIni));
           panel.getChildren().add(l);
         }
       }
     });
     panel.setOnMouseDragged((MouseEvent e) -> {
       if(tipo == 2){
-        int ind_noFim = lnos.sobre_circ(e.getX(), e.getY());
+        int ind_noFim = grafo.sobre_circ(e.getX(), e.getY());
         if(ind_noFim != ind_noIni && ind_noFim != -1){
-          l.setEndX(lnos.posX(ind_noFim));
-          l.setEndY(lnos.posY(ind_noFim));
-          l.setStrokeWidth(tam/10);
-          l.setStroke(cor.getValue()); 
-          Circle temp = lnos.get_circle(ind_noFim);
-          c = new Circle(temp.getCenterX(), temp.getCenterY(), temp.getRadius(), temp.getFill());
-          c.setStroke(temp.getStroke());
+          editline(grafo, ind_noFim, cor);
         }
         else if(ind_noIni != -1){
-          l.setEndX(lnos.posX(ind_noIni));
-          l.setEndY(lnos.posY(ind_noIni));
+          l.setEndX(grafo.posX(ind_noIni));
+          l.setEndY(grafo.posY(ind_noIni));
         }
       }
     });
     panel.setOnMouseReleased((MouseEvent e) -> {
       if(tipo == 2){
         if(l.getStartX() != l.getEndX() && l.getStartY() != l.getEndY())
-          lline.add_line(l);
+          grafo.add_line(l);
       }
     });
   }
   public void changeTam(double t){
     tam = t;
+  }
+  
+  public void editline(Grafo grafo, int ind_noFim, ColorPicker cor){
+    l.setEndX(grafo.posX(ind_noFim));
+    l.setEndY(grafo.posY(ind_noFim));
+    l.setStrokeWidth(tam/10);
+    l.setStroke(cor.getValue()); 
+    Circle temp = grafo.get_circle(ind_noFim);
+    c = new Circle(temp.getCenterX(), temp.getCenterY(), temp.getRadius(), temp.getFill());
+    c.setStroke(temp.getStroke());
   }
 }
